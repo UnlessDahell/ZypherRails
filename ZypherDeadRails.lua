d2feba419d0988019ece0a3aca76a66ea5baaa67
UnlessDahell <148602200+UnlessDahell@users.noreply.github.com>
2025-03-29 20:41:13 +0700

    ZypherDeadRails.lua

--- ZypherDeadRails.lua
+++ ZypherDeadRails.lua
@@ -1,1 +1,30 @@
-print("nigga its not working tru using getsolara.dev for free rat/virus")
+local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/main/source.lua", true))()
+
+local Window = Luna:CreateWindow({
+	Name = "Zypher Hub (Luna UI)", 
+	Subtitle = "By Maam.Zypher and Eyes Sight", 
+	LogoID = "82795327169782", 
+	LoadingEnabled = true, 
+	LoadingTitle = "Wait Until Luna UI Load Up", 
+	LoadingSubtitle = "By Maam.Zypher and Eyes Sight", 
+
+	ConfigSettings = {
+		RootFolder = "Zypher", 
+		ConfigFolder = "ZypherConfig" 
+	},
+
+	KeySystem = false, 
+	KeySettings = {
+		Title = "Keysystem",
+		Subtitle = "Key System",
+		Note = "There no key buddy",
+		SaveInRoot = true, 
+		SaveKey = true, 
+		Key = {"Key"}, 
+		SecondAction = {
+			Enabled = true, 
+			Type = "https://discord.gg/ere8H6q9", 
+			Parameter = ".gg/ere8H6q9" 
+		}
+	}
+})
